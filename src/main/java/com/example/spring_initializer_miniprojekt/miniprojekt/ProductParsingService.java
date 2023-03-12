ab67bf8f33267c67764d091634cd8aae3704db0c
Tomasz Kocyła <kocyla.t@gmail.com>
2023-03-12 16:01:40 +0100

    Implement class ProductParsingService

--- src/main/java/com/example/spring_initializer_miniprojekt/miniprojekt/ProductParsingService.java
+++ src/main/java/com/example/spring_initializer_miniprojekt/miniprojekt/ProductParsingService.java
@@ -1,7 +1,15 @@
 package com.example.spring_initializer_miniprojekt.miniprojekt;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProductParsingService {
+    private ProductRepository productRepository1;
+    private ProductRepository productRepository2;
+
+    public ProductParsingService(ProductRepository productRepository, ProductRepository productRepository2) {
+        this.productRepository1 = productRepository;
+        this.productRepository2 = productRepository;
+    }
 }
--- src/main/java/com/example/spring_initializer_miniprojekt/miniprojekt/ProductRepositoryCSV.java
+++ src/main/java/com/example/spring_initializer_miniprojekt/miniprojekt/ProductRepositoryCSV.java
@@ -1,7 +1,13 @@
 package com.example.spring_initializer_miniprojekt.miniprojekt;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ProductRepositoryCSV {
+@Primary
+public class ProductRepositoryCSV implements ProductRepository {
+    @Override
+    public void parse() {
+
+    }
 }

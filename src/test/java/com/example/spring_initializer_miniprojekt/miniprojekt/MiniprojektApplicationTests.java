d1e76c73a1f80db7a5505a04679d195ab0ef14c7
Maciej Lipinski <maciej.lipinski123@gmail.com>
2023-03-12 15:29:45 +0100

    initial commit

---
+++ src/test/java/com/example/spring_initializer_miniprojekt/miniprojekt/MiniprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_initializer_miniprojekt.miniprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniprojektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
